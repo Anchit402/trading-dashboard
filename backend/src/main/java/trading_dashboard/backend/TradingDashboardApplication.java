f7704d41829dbc5bc65758032e022007ddc5e9dc
anchit27 <anchit.agarwal@pocketpills.com>
2025-08-30 14:29:13 +0530

    initialized spring project

---
+++ backend/src/main/java/trading_dashboard/backend/TradingDashboardApplication.java
@@ -0,0 +1,13 @@
+package trading_dashboard.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingDashboardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/trading_dashboard/backend/TradingDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package trading_dashboard.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
