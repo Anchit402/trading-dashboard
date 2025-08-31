15360f966658ca4a587211c9c983d081c67dc7fd
anchit27 <anchit.agarwal@pocketpills.com>
2025-08-31 17:41:28 +0530

    added routing and navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
   return (
     <>
-      
+      <div className="w-full flex justify-center mt-4">
+        <div className="w-95/100">
+          <Navbar />
+          <Outlet />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Orders from './components/Orders/Orders'
+import Home from './components/Home/Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/', 
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      },
+      {
+        path: '/orders',
+        element: <Orders />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
